6be579b8700ab00f88023aec2bf36e023fe5c632
barryailun <15452938282@qq.com>
2019-10-20 22:38:38 +0800

    mod MyLinkedList

--- src/main/java/top/sstime/algorithm/MyLinkedList.java
+++ src/main/java/top/sstime/algorithm/MyLinkedList.java
@@ -62,11 +62,46 @@ public class MyLinkedList<T> implements Iterable<T> {
         addBefore(getNode(idx, 0, size()), x);
     }
 
+    /**
+     * 通过获取一个新的节点，然后按所指示的顺序改变指针而完成一个双链表的插入操作
+     * @param node
+     * @param x
+     */
     private void addBefore(Node<T> node, T x) {
+        Node<T> newNode = new Node<>(x, node.prev, node);
+        newNode.prev.next = newNode;
+        node.prev = newNode;
+
+        theSize++;
+        modCount++;
     }
 
-    private Node<T> getNode(int idx, int i, int size) {
-        return null;
+    private T remove(Node<T> p) {
+        p.next.prev = p.prev;
+        p.prev.next = p.next;
+        theSize--;
+        modCount++;
+        return p.data;
+    }
+
+    private Node<T> getNode(int idx) {
+        return getNode(idx, 0, size() - 1);
+    }
+
+    private Node<T> getNode(int idx, int lower, int upper) {
+        Node<T> p;
+        if (idx < lower || idx > upper)
+            throw new IndexOutOfBoundsException();
+        if (idx < size() /2) {
+            p = beginMarker.next;
+            for (int i = 0; i < idx; i++)
+                p = p.next;
+        } else {
+            p = endMarker;
+            for (int i = size(); i > idx; i--)
+                p = p.prev;
+        }
+        return p;
     }
 
     @Override
